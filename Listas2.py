b43dc8150b8a7d0e463cd39163f34fd300cde5de
Amanda Brízido dos Santos <brizidoamanda@gmail.com>
2025-10-01 13:55:35 -0300

    Create Listas2.py

---
+++ Listas2.py
@@ -0,0 +1,8 @@
+
+vocabulario = ["iteracao", "selecao", "controle"]
+numeros = [ 17, 123]
+lista_mista = ["ola",2.0, 5*2, [17, 123]]
+print (numeros)
+print (lista_mista)
+nova_lista = [numeros,vocabulario]
+print (nova_lista)
